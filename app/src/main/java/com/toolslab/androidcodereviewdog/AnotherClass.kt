e456c6729d40b2bf73b5e030fc25df772856c90d
Sebastian Guillen <code-schreiber@users.noreply.github.com>
2020-02-28 16:25:08 +0100

    Add basic setup for reviewdog with detekt (#2)
    * Add basic setup for reviewdog working with detekt and Github checks
    * Set level of all reviewdog checks to warning since it doesn't differentiate errors/warnings from report

---
+++ app/src/main/java/com/toolslab/androidcodereviewdog/AnotherClass.kt
@@ -0,0 +1,11 @@
+package com.toolslab.androidcodereviewdog
+
+
+import androidx.appcompat.app.AppCompatActivity
+
+
+class AnotherClass {
+
+    val WRONG_mixedFORMAT = "WRONG_mixedFORMAT"
+
+}
---
+++ app/src/main/java/com/toolslab/androidcodereviewdog/MainActivity.kt
@@ -0,0 +1,20 @@
+package com.toolslab.androidcodereviewdog
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+
+    val unusedVariable = 0
+
+    val WRONG_FORMAT = "WRONG_FORMAT"
+
+    // Comment ending in a space
+
+    private val unusedPrivateVal = 1.2
+
+}
+
+
+
+
